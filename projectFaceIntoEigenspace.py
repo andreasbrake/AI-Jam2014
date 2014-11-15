9c6f0f564993eb4d280955dbf2b335a6b18627a7
Lifesci <none>
2014-11-15 17:28:39 -0500

    Function to project the test face into the facespace and determine proximity

---
+++ projectFaceIntoEigenspace.py
@@ -0,0 +1,12 @@
+import os, glob
+import numpy as np
+import dataHandler as db
+from PIL import Image
+
+# function to calculate the projections of the training images and and the test image
+# in the face space (eigen faces)
+def trainingProjections(testImage, faceSpace, trainingSpace):
+    
+
+if __name__ == '__main__':
+    trainingProjections()
