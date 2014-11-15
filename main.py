8896b05938bb35774d8513af569d0f098949fcc9
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2014-11-15 15:55:21 -0500

    Returns error in case of invalid input and added a main function.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+from __future__ import print_function
 import sys, os
 
-# user should pass one file relative to the current directory
-if len(sys.argv) == 2:
-	pathname = os.path.abspath(sys.argv[1])
+def main():
+	# user should pass one file relative to the current directory
+	if len(sys.argv) == 2:
+		pathname = os.path.abspath(sys.argv[1])
 	
-	if os.path.isfile(pathname):
-		# TODO call our algorithm with path to calculate closest match
-		print "path =", pathname
+		if os.path.isfile(pathname):
+			# TODO call our algorithm with path to calculate closest match
+			print("path =", pathname)
+		else:
+			print("Error: must pass valid file.\n", file=sys.stderr)	
 	else:
-		print "Error: must pass valid file.\n"	
-else:
-	print "Error: must pass the path to an image filename."
-	print "Usage: python main.py blah.gif\n"
+		print("Error: must pass the path to an image filename.", file=sys.stderr)
+		print("Usage: python main.py blah.gif\n")
+
+if __name__ == "__main__":
+	main()
+
