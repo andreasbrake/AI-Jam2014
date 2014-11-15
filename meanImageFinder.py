76dc275955d8f139755918b6a698ed5959f46206
andreasbrake <andreas@andreasbrake.ca>
2014-11-15 11:51:15 -0500

    Added extra code up to getting a cov matrix
    - cov throws a memory error

--- meanImageFinder.py
+++ meanImageFinder.py
@@ -1,11 +1,5 @@
-#im = Image.open("./img/1_2_.gif")
-
-#im.save("./img/testOut.gif", "GIF")
-
-import os, numpy, PIL
+import os, numpy, PIL, time
 from PIL import Image
-
-# Access all PNG files in directory
 
 def getMeanOf(imgId):
     allfiles=os.listdir(os.getcwd() + "\\img\\")
@@ -22,18 +16,39 @@ def getMeanOf(imgId):
     arr=numpy.zeros((h,w),numpy.float)
 
     # Build up average pixel intensities, casting each image as an array of floats
+    origArrays = []
+    bigArray = []
+
     for im in imlist:
         imarr=numpy.array(Image.open(im),dtype=numpy.float)
         arr=arr+imarr/N
+        imarr = numpy.resize(imarr, (1, 77760))
 
-    # Round values in array and cast as 8-bit integer
+        if len(bigArray) == 0:
+            bigArray = imarr
+        else:
+            bigArray = numpy.append(bigArray, imarr, axis=0)
+
+        origArrays.append(imarr)
+
+    bigArray = numpy.array(numpy.round(bigArray),dtype=numpy.uint8)
     arr=numpy.array(numpy.round(arr),dtype=numpy.uint8)
+    
+    flatMean = numpy.resize(arr, (1, 77760))
+
+    for i in range(0, len(bigArray)):
+        bigArray[i] = bigArray[i] - flatMean
+
+    bigArray = bigArray.T
+
+    print numpy.cov(bigArray)
 
     # Generate, save and preview final image
     out=Image.fromarray(arr)
-    print "creating mean at: " + "./img/" + imgId + "Mean.gif"
+
+    #print "creating mean at: " + "./img/" + imgId + "Mean.gif"
     out.save("./img/" + imgId + "Mean.gif","GIF")
 
 
-for i in range(1,16):
+for i in range(1,2):
     getMeanOf(str(i) + "_")
